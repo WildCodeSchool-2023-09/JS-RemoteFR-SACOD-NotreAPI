1c53be0b0b891b6313df3be28e33b439a819d065
Samuel Faber <samuel.faber@live.fr>
2023-10-24 15:40:46 +0200

    Commentaires sur le script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,32 @@
-console.log("Coucou les SACOD");
+// console.log("Coucou les SACOD");
 
-fetch("http://localhost:5500/")
+/**
+ * Nous allons avoir notre base API
+ */
+
+const BASE_API = "http://localhost:5500/";
+
+/**
+ * doc: fetch
+ * https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+ */
+fetch(BASE_API)
   .then((response) => response.json())
   .then((data) => {
     /**
      * Toute notre logique du DOM
      */
+    // Je récupère mon ul depuis le DOM
     const ul = document.querySelector("ul");
 
     for (let i = 0; i < data.length; i++) {
+      // Je créer une balise li
       const li = document.createElement("li");
+      // Je lui injecte la data via `.innerText`
       li.innerText = data[i].name + " apprends le " + data[i].learn;
-      // En dernier
+      // J'ajoute ensuite ce li en tant qu'enfant de mon `ul`
       ul.appendChild(li);
     }
   });
+
+// Et voilà 🎊
