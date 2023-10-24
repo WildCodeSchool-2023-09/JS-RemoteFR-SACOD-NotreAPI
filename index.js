9187967bcf916b2b1957edc8ef3c0a98f4ecb988
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-24 10:10:51 +0200

    add server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+const PORT = 5500;
+
+app.listen(PORT, (err) => {
+	if (err) {
+		console.info("Oups, une erreur est servenu: ", err);
+	} else {
+		console.info(
+			`Le serveur est lancé sur le port ${PORT}. Accès http://localhost:${PORT}`
+		);
+	}
+});
